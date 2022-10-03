b181c1148e9ff3bea6736248a7dc3652ddfe49bb
Ramide Dantas <ramidedantas@recife.ifpe.edu.br>
2022-10-03 16:05:18 -0300

    Commit resolvido

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,7 +3,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World!!!!!!!");
-		System.out.println("Nova linha!");
+		System.out.println("Nova linha - merge!");
 	}
 
 }
